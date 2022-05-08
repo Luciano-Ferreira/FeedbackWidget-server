71ad40e439997c3baac4c396e30be6c2c7e3c9d8
Luciano Ferreira <luciano2023silva@gmail.com>
2022-05-08 01:29:43 -0300

    stage 2 :rocket:

---
+++ README.md
@@ -0,0 +1,15 @@
+# SOLID
+
+1. Single Responsibility Principle
+2. Open/Closed Principle
+3. Liskov Substitution Principle
+4. Interface Segregation Principle
+5. Dependency Inversion Principle
+
+<hr>
+
+1. Cada classe deve ter apenas uma responsabilidade
+2. Classes abertas devem ser abertas para extensão mas fechadas para modificação
+3. Nós devemos poder substituir uma classe pai por uma herança dela e tudo continuar funcionando.
+4. ...
+5. Princípio de Inversão de Dependência
---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer';
+
+import { MailAdapter, SendMailData } from '../mail-adapter';
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '989b97a749a7c4',
+    pass: '8f3e8656dafd3d',
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Luciano Silva <luciano2023silva@gmail.com>',
+      subject,
+      html: body,
+    });
+  }
+}
