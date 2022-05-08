3a2a5ccac3d02ea6c9715ac817b266d94f969a77
Luciano Ferreira <luciano2023silva@gmail.com>
2022-05-08 17:01:28 -0300

    add handler error in route

--- src/routes.ts
+++ src/routes.ts
@@ -6,20 +6,26 @@ import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 export const routes = express.Router();
 
 routes.post('/feedbacks', async (req, res) => {
-  const { type, comment, screenshot } = req.body;
+  try {
+    const { type, comment, screenshot } = req.body;
 
-  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
-  const nodemailerMailAdapter = new NodemailerMailAdapter();
-  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-    prismaFeedbacksRepository,
-    nodemailerMailAdapter
-  );
+    const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+    const nodemailerMailAdapter = new NodemailerMailAdapter();
+    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+      prismaFeedbacksRepository,
+      nodemailerMailAdapter
+    );
 
-  await submitFeedbackUseCase.execute({
-    type,
-    comment,
-    screenshot,
-  });
+    await submitFeedbackUseCase.execute({
+      type,
+      comment,
+      screenshot,
+    });
 
-  return res.status(201).send();
+    return res.status(201).send();
+  } catch (err) {
+    console.error(err);
+
+    return res.status(500).send();
+  }
 });
